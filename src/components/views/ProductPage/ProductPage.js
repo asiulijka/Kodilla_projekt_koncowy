559c7aed851ec67a5eed85a939cbca9d1fd54690
asiulijka <j.m.h.zielinska@gmail.com>
2021-12-13 17:32:15 +0100

    wip Product Page - early stage, basic photo and description setups

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -7,41 +7,41 @@ import clsx from 'clsx';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './ProductPage.module.scss';
-import { Col, Row, Container } from 'react-bootstrap';
+// import { Col, Row, Container } from 'react-bootstrap';
 
 const Component = ({className, children}) => (
   <div className={clsx(className, styles.root)}>
     {/* <h2>ProductPage</h2>
     <p>This subpage is under construction</p> */}
 
-    <Container fluid>
-      <Row>
+    <div className="container">
+      <div className="row">
 
 
-        <Col className='styles.pageColumnLeft'>
+        <div className={'col-5 ' + styles.pageColumnLeft}>
 
           <div className={styles.photoContainer}>
-            <p>Product Photo here</p>
-            {/* <img className={styles.photo} src={''} alt='' />
-            <Button className={styles.expandButton} variant='outline'>
+            {/* <p>Main Product Photo here</p> */}
+            <img className={styles.photo} src={'https://images.pexels.com/photos/68704/pexels-photo-68704.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'} alt='' />
+            {/* <Button className={styles.expandButton} variant='outline'>
               <FontAwesomeIcon icon={faExpandArrowsAlt}></FontAwesomeIcon>
-            </Button> */}
+            </Button> */ }
           </div>
 
           <div className={'row ' + styles.sliderBox}>
-            <p>Slider box - mini photos</p>
-            {/* <div className={'col ' + styles.photoContainerMini}>
+            {/* <p>Slider box - mini photos</p> */}
+            <div className={'col-2 ' + styles.photoContainerMini}>
+              <img className={styles.photoMini} src={'https://images.pexels.com/photos/2193300/pexels-photo-2193300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'} alt='' />
+            </div>
+            <div className={'col-2 ' + styles.photoContainerMini}>
+              <img className={styles.photoMini} src={'https://images.pexels.com/photos/2122423/pexels-photo-2122423.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'} alt='' />
+            </div>
+            <div className={'col-2 ' + styles.photoContainerMini}>
+              <img className={styles.photoMini} src={'https://images.pexels.com/photos/1619854/pexels-photo-1619854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'} alt='' />
+            </div>
+            <div className={'col-2 ' + styles.photoContainerMini}>
               <img className={styles.photoMini} src={''} alt='' />
             </div>
-            <div className={'col ' + styles.photoContainerMini}>
-              <img className={styles.photoMini} src={''} alt='' />
-            </div>
-            <div className={'col ' + styles.photoContainerMini}>
-              <img className={styles.photoMini} src={''} alt='' />
-            </div>
-            <div className={'col ' + styles.photoContainerMini}>
-              <img className={styles.photoMini} src={''} alt='' />
-            </div> */}
 
             {/* <div className={'col-12 ' + styles.sideArrow}>
               <Button variant='sideArrows'>
@@ -52,38 +52,40 @@ const Component = ({className, children}) => (
               </Button>
             </div> */}
           </div>
-        </Col>
+        </div>
 
 
-        <Col className='styles.pageColumnRight'>
+        <div className={'col-7 ' + styles.pageColumnRight}>
 
-          <div className={styles.productInfoContainer}>
-            <div className={'row ' + styles.productIntro}>
-              <h4>Bottle for sale</h4>
-              {/* <div className={'row ' + styles.productIntro}>
-                <div className={styles.buttonContainer}>
-                  <Button variant='sideArrows'>
-                    <FontAwesomeIcon icon={faChevronLeft} />
-                  </Button>
-                </div>
-                <div className={styles.buttonContainer}>
-                  <Button variant='sideArrows'>
-                    <FontAwesomeIcon icon={faChevronRight} />
-                  </Button>
-                </div>
-              </div> */}
+          <div className={'container ' + styles.productInfoContainer}>
+            <div className='row'>
+              <div className={'col ' + styles.productIntro}>
+                <h4>Bottle for sale</h4>
+              </div>
+              <div className={'col ' + styles.productIntro}>
+                <p>Price from $5.00 each</p>
+              </div>
             </div>
-            {/* <div className={'row ' + styles.starsRow}> */}
-            {/* <StarRating stars={stars} id={id} userRating={userRating} /> */}
-            {/* <p>(0 reviews)</p> */}
-            {/* <p>Add Your Review</p> */}
-            {/* </div> */}
           </div>
 
-          <div className={'row ' + styles.productInfoContainer}>
-            <p>Old and new price</p>
-            {/* <div className={styles.oldPrice}>$350.00</div>
-            <div className={styles.newPrice}>$250.00</div> */}
+          <div className={'container ' + styles.productInfoContainer}>
+            <div className='row'>
+              <div className={'col ' + styles.availabilityContainer}>
+                <p><strong>Category:</strong> Bottles</p>
+              </div>
+              <div className={'col ' + styles.availabilityContainer}>
+                <p>Availability: In Stock</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.productInfoContainer}>
+            <p className={styles.overview}><strong>Description</strong></p>
+            <p className={styles.description}>
+              Pellentesque dui quam, finibus et venenatis sed, tristique vitae nisi.
+              Pellentesque gravida erat in pretium ultrices. In auctor arcu eget leo
+              rutrum, non laoreet lorem dignissim.
+            </p>
           </div>
 
           <div className={styles.productInfoContainer}>
@@ -120,26 +122,6 @@ const Component = ({className, children}) => (
               <div className={styles.buttonContainer}>
                 <Button variant='quantityChange'>+</Button>
               </div> */}
-            </div>
-          </div>
-
-          <div className={styles.productInfoContainer}>
-            <p className={styles.overview}>Quick Overview</p>
-            <p className={styles.description}>
-              Pellentesque dui quam, finibus et venenatis sed, tristique vitae nisi.
-              Pellentesque gravida erat in pretium ultrices. In auctor arcu eget leo
-              rutrum, non laoreet lorem dignissim.
-            </p>
-          </div>
-
-          <div className={styles.productInfoContainer}>
-            <div className={'row ' + styles.availabilityContainer}>
-              <p>Availability:</p>
-              <div>In Stock</div>
-            </div>
-            <div className={'row ' + styles.availabilityContainer}>
-              <p>Category:</p>
-              <div>Bottles</div>
             </div>
           </div>
 
@@ -190,10 +172,10 @@ const Component = ({className, children}) => (
               </Button> */}
             </div>
           </div>
-        </Col>
+        </div>
       
-      </Row>
-    </Container>
+      </div>
+    </div>
   </div>
 );
 
