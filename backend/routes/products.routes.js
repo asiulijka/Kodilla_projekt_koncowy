97c9d377ed7264c35494653c9a5c41469ba8e134
asiulijka <j.m.h.zielinska@gmail.com>
2021-12-24 02:31:19 +0100

    init backend and mongoose

---
+++ backend/routes/products.routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+// const sanitize = require('mongo-sanitize');
+
+const Product = require('../models/product.model');
+
+
+
+router.get('/products', async (req, res) => {
+  try {
+    const result = await Product.find();
+    if(!result) res.status(404).json({ product: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/products/:id', async (req, res) => {
+  try {
+    const result = await Product
+      .findById(req.params.id);
+    if(!result) res.status(404).json({ product: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
+module.exports = router;
